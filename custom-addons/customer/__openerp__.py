7d8ac07a263887890b38010e68ef9bf6aa86778a
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-10-13 09:10:48 -0500

    [ADD] Initial version

---
+++ custom-addons/customer/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#  Customer Data and Configuration module 
+#  Copyright (C) 2012 - TODAY, Ursa Information Systems
+#  Website:(<http://www.ursainfosystems.com>).
+#
+#  This program is free software: you can redistribute it and/or modify it 
+#  under the terms of the GNU Affero General Public License as published by 
+#  the Free Software Foundation, either version 3 of the License, or (at 
+#  your option) any later version.
+#
+#  For clients with an annual support contract with Ursa, this program is
+#  warranted within the guidelines of that contract.
+#
+#  For ALL OTHERS, this program is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY (including the absence of implied 
+#  warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE).  See 
+#  the GNU Affero General Public License for more information.
+#
+###############################################################################
+
+{
+    'name': 'Customer',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'summary': 'Customer Configuration and Data',
+    'author': 'Ursa Information Systems',
+    'maintainer': 'Ursa Information Systems',
+    'website': 'http://www.ursainfosystems.com',
+    'depends': [
+        'auditlog',
+        'auto_backup',
+    ],
+    'data': [],
+    'application': True,
+}
