aa5333d92d1a015cda2cb4e4779380a18197fc67
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-05-11 15:44:57 -0500

    [IMP] Documentation

--- custom-addons/customer/README.rst
+++ custom-addons/customer/README.rst
@@ -1,12 +1,29 @@
 .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
-   :alt: License: AGPL-3
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
 
 ========
 CUSTOMER
 ========
 
-This module provides the scope (dependencies), configuration and data of Odoo for CUSTOMER
+This module provides the scope (dependencies), configuration and data of Odoo for CUSTOMER.
+
+Installation
+============
+
+* No specific installation step required.
+
+Configuration
+=============
+
+* No specific configuration step required.
+
+Usage
+=====
+
+To use this module
+
+* Go to ...
 
 Credits
 =======
--- custom-addons/customer/__manifest__.py
+++ custom-addons/customer/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Customer',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'summary': 'Customer Configuration and Data',
-    'author': 'Ursa Information Systems',
-    'maintainer': 'Ursa Information Systems',
-    'website': 'http://www.ursainfosystems.com',
-    'depends': [
+    "name": "Customer",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Customer Configuration and Data",
+    "author": "Ursa Information Systems",
+    "maintainer": "Ursa Information Systems",
+    "website": "http://www.ursainfosystems.com",
+    "depends": [
     ],
-    'data': [
+    "data": [
     ],
-    'application': True,
+    "application": True,
 }
