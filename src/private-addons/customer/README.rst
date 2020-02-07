fa07b1e259d62ce44daaf16ec8c8188cecf5d68b
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-02-07 14:08:29 -0600

    [ADD] Customer module

---
+++ src/private-addons/customer/README.rst
@@ -0,0 +1,20 @@
+========
+Customer
+========
+
+This module provides the scope (dependencies), configuration and data of Odoo for Customer.
+
+Usage
+=====
+
+* Go to ???
+
+Credits
+=======
+
+Contributors
+------------
+
+* Open Source Integrators <https://www.opensourceintegrators.com>
+
+  * Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
---
+++ src/private-addons/customer/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2018 Open Source Integrators
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Customer",
+    "summary": "Customer Configuration and Data",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://www.opensourceintegrators.com",
+    "depends": [
+        # Odoo Addons
+        "board",
+        "calendar",
+        "contacts",
+        # OCA Addons
+        "web_responsive",
+        # Private Addons
+    ],
+    "data": [],
+    "application": True,
+    "sequence": 0,
+}
