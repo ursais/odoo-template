0c2e4e21b8910a162a4fbbbe93a48eabfdb37ff7
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-11-13 15:34:52 -0600

    [IMP] customer: version

--- src/custom-addons/customer/__manifest__.py
+++ src/custom-addons/customer/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Customer",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Customer Configuration and Data",
     "author": "Open Source Integrators",
