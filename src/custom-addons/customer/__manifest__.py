c45afa60bd9b600c5d4d45dc85742d0f7e1d42a8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-11-13 14:09:56 -0600

    [FIX] customer module

--- src/custom-addons/customer/__manifest__.py
+++ src/custom-addons/customer/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 Open Source Integrators (https://www.opensourceintegrators.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -14,5 +13,6 @@
     ],
     "data": [
     ],
+    "sequence": 0,
     "application": True,
 }
