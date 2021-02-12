85427beb78f764e60db92e45c3292f11d7919a77
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-02-11 19:20:42 -0600

    Update __manifest__.py

--- src/custom-addons/customer/__manifest__.py
+++ src/custom-addons/customer/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 Open Source Integrators (https://www.opensourceintegrators.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Customer",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Customer Configuration and Data",
     "author": "Open Source Integrators",
     "maintainer": "Open Source Integrators",
-    "website": "http://www.opensourceintegrators.com",
+    "website": "https://www.opensourceintegrators.com",
     "depends": [
+        "discuss",
     ],
     "data": [
     ],
