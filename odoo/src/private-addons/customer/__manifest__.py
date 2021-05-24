471613d4f6d758c3eb36bf3aa964d9db792f853d
jevin <jdement16@gmail.com>
2021-05-24 13:34:37 -0700

    [fix] 12.0 odoo template: submodules and test

---
+++ odoo/src/private-addons/customer/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2017 Open Source Integrators (https://www.opensourceintegrators.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Customer",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Customer Configuration and Data",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://www.opensourceintegrators.com",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+    ],
+    "application": True,
+}
