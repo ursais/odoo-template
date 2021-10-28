a2360e0a82f819cc0176614a08691b2a24bf8c74
Hiren Pattani-SerpentCS <hiren.pattani@serpentcs.com>
2021-10-28 20:56:53 +0530

    [ADD] Added the odoo 15 template

--- odoo/src/private-addons/customer/__manifest__.py
+++ odoo/src/private-addons/customer/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Customer",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Customer Configuration and Data",
     "author": "Open Source Integrators",
