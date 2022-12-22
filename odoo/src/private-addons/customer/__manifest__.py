b5256966658d04738879253e47130e006d96549c
Daniel Reis <dreis@opensourceintegrators.com>
2022-12-22 16:53:57 +0000

    [REF] pre-commit: fix lint errors

--- odoo/src/private-addons/customer/__manifest__.py
+++ odoo/src/private-addons/customer/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2017 Open Source Integrators (https://www.opensourceintegrators.com)
+# Copyright (C) 2022 Open Source Integrators (https://www.opensourceintegrators.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Customer",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Customer Configuration and Data",
     "author": "Open Source Integrators",
--- odoo/src/private-addons/elearning_content/__manifest__.py
+++ odoo/src/private-addons/elearning_content/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Elearning Content",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Training Content and Documentation",
     "author": "Open Source Integrators",
